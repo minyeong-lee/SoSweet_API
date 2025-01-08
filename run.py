94e02296b386be5ce7f6a2f9786ef94425b6ee31
dnwls6102 <dnwls6102@gmail.com>
2025-01-09 03:50:28 +0900

    [feat]충돌 해결

--- run.py
+++ run.py
@@ -9,13 +9,6 @@ if __name__ == "__main__":
     
     app.run(host="0.0.0.0",
             port=5000, 
-<<<<<<< HEAD
-            debug=True,
-            # ssl_context=(
-            #     'C:/AWS/SoSweet.pem',
-            # )  # 인증서 경로 설정
-=======
             # debug=True,
             # ssl_context=ssl_ctx  # 인증서 경로 설정
->>>>>>> 97bd816c25e23c824f399d3aca0e72c55c84756e
     )
