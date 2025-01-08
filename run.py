83c3662c9fd777945101da8e75deb70ab7fc4ce2
minyeong-lee <lmy1366@gmail.com>
2025-01-09 01:21:03 +0900

    [chore] pem 파일 설정

--- run.py
+++ run.py
@@ -3,4 +3,10 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0",
+            port=5000, 
+            # debug=True,
+            ssl_context=(
+                'C:/AWS/SoSweet.pem',
+            )  # 인증서 경로 설정
+    )
