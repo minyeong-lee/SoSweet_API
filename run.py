0526c03d11b12bad7b001273177baca649d2c150
minyeong-lee <lmy1366@gmail.com>
2025-01-09 03:35:17 +0900

    [fix] MediaPipe 프레임 오류 해결

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ app = create_app()
 if __name__ == "__main__":
     app.run(host="0.0.0.0",
             port=5000, 
-            # debug=True,
-            ssl_context=(
-                'C:/AWS/SoSweet.pem',
-            )  # 인증서 경로 설정
+            debug=True,
+            # ssl_context=(
+            #     'C:/AWS/SoSweet.pem',
+            # )  # 인증서 경로 설정
     )
