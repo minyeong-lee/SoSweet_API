bada32080bbd4ac481de57518e28e5a20594fde9
dnwls6102 <dnwls6102@gmail.com>
2025-01-09 03:18:40 +0900

    [fix]pem 수정중

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
+import ssl
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
+    ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_ctx.load_cert_chain('/home/ubuntu/SoSweet_API.pem')  
+    
     app.run(host="0.0.0.0",
             port=5000, 
             # debug=True,
-            ssl_context='/home/ubuntu/SoSweet_API.pem'  # 인증서 경로 설정
+            ssl_context=ssl_ctx  # 인증서 경로 설정
     )
